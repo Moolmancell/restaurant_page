8d8573f3ca17aeeba21fcefc4c131fc7a56fa8b0
Moolmancell <richmondclaudebodot@gmail.com>
2024-07-29 15:12:23 +0800

    Add JS files

---
+++ src/images.js
@@ -0,0 +1,6 @@
+import vegetables from './images/vegetables-picture.png'
+import leaf1 from './images/leaf1.png';
+import leaf2 from './images/leaf2.png';
+import leaf3 from './images/leaf3.png'
+
+document.getElementById('vegetable-picture').src = vegetables
